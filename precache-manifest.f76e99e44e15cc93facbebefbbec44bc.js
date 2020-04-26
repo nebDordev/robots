69c8f5f984decc0780aa7a07f752bd2d0e4de5ce
nebDordev <60310841+nebDordev@users.noreply.github.com>
2020-04-26 17:39:37 +1200

    Updates

---
+++ precache-manifest.f76e99e44e15cc93facbebefbbec44bc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e240910e6b79989af3dd7b649f0ab9bd",
+    "url": "/robots/index.html"
+  },
+  {
+    "revision": "5bf1e14fae26528cb71c",
+    "url": "/robots/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "ac080afa198f07704307",
+    "url": "/robots/static/css/main.70b2709a.chunk.css"
+  },
+  {
+    "revision": "5bf1e14fae26528cb71c",
+    "url": "/robots/static/js/2.c542e2fd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robots/static/js/2.c542e2fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac080afa198f07704307",
+    "url": "/robots/static/js/main.48bc208e.chunk.js"
+  },
+  {
+    "revision": "69d94ad12ca66e1a8ecb",
+    "url": "/robots/static/js/runtime-main.f1d8377f.js"
+  }
+]);
